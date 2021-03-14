515a15644ce6d6e6c5bf850b2bf76403aab6fb97
rGradov <VladRudenualu@yandex.by>
2021-03-14 17:57:19 +0300

    #4 making hash password

--- src/modules/admin/entity/admin.entity.ts
+++ src/modules/admin/entity/admin.entity.ts
@@ -1,10 +1,20 @@
-import { Entity, ObjectID, ObjectIdColumn, Column } from 'typeorm';
+import {
+  Entity,
+  ObjectID,
+  ObjectIdColumn,
+  Column,
+  BeforeInsert,
+} from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Entity('admin')
 export class Admin {
   @ObjectIdColumn() id: ObjectID;
   @Column() email: string;
   @Column() password: string;
+  @BeforeInsert() async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
   constructor(admin?: Partial<Admin>) {
     Object.assign(this, admin);
   }
